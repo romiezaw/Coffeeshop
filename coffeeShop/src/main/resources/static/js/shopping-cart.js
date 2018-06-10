fada84a5242455cf978a1ca2eaf6966b50d03b36
romiezaw <ymzaw@mum.edu>
2018-06-10 18:31:14 -0500

    Shopping Cart Fix

--- coffeeShop/src/main/resources/static/js/shopping-cart.js
+++ coffeeShop/src/main/resources/static/js/shopping-cart.js
@@ -1,13 +1,13 @@
 $(function() {
 	console.log("in shopping cart");
-    $("#placeOrder-btn").click(function(e) {
-    	console.log("in shopping cart..");
-        $.ajax({
-            type: "POST",
-            url: "api/placeOrder",
-            contentType: "application/json; charset=utf-8",
-            success: function() {
-            }
-        });
-    });
+//    $("#placeOrder-btn").click(function(e) {
+//    	console.log("in shopping cart..");
+//        $.ajax({
+//            type: "POST",
+//            url: "api/placeOrder",
+//            contentType: "application/json; charset=utf-8",
+//            success: function() {
+//            }
+//        });
+//    });
 }); 
